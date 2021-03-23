09b7edfceb820c1ae09db43a910897ebfcc572a4
Daniel Noble <daniel.wa.noble@gmail.com>
2021-03-23 13:51:03 +1100

    Cleaning data, checking and exploring

--- R/data_prep.R
+++ R/data_prep.R
@@ -6,13 +6,11 @@
 # Libraries
 pacman::p_load(tidyverse, metafor, brms)
 
-
 # Load data written from phylogeny script. 
 	data <- read.csv("./data/meta_data.csv")
 	str(data)
 
 # Convert all se to SD
-
 	data <- data %>%
 			mutate(sd_T = ifelse(Err_T_Type == "se", Err_T*sqrt(N_T), Err_T),
 				   sd_C = ifelse(Err_C_Type == "se", Err_C*sqrt(N_C), Err_C),
@@ -26,4 +24,29 @@ pacman::p_load(tidyverse, metafor, brms)
 	plot(log(X_C) ~ log(sd_C), data = data)
 	with(data, text(obs, y = log(X_C)+0.5, x=log(sd_C)+0.05, cex = 0.85)) 
 
-# 
+# Explore a bit more of how much data we have across various moderators
+	data %>% 
+	select_if(is.factor) %>% 
+	select(-c(notes, Dependency_decisions, journal, pg, author, estimated.increase.in.harm, Err_C_Type, Err_T_Type, C_flipped, source)) %>% 
+	glimpse %>% summary(.)
+
+# From this it is quite clear that there is not too much use in Parental Care as most don't have care. Most of the data is a direct measure of fitness, which is good. Most data are from oviparous, but sensible split possibly.
+
+# Explore species level data
+	spp_data <- data.frame(data %>%
+	group_by(species) %>% 
+	summarise(Parental.care = unique(Parental.care),
+			  Gestation     = unique(Gestation),
+			  Sperm.compet  = unique(Sperm.compet),
+			  SCR.SCI 		= unique(SCR.SCI),
+			  treatment     = unique(treatment)) )
+
+	spp_data %>% summary()
+
+
+# Generate Effect Sizes - Going to use SMDH (Hedges g with heteroscedasticity correction)
+data <- escalc(m1i = X_C, sd1i =sd_C, n1i = N_C, m2i= X_T, sd2i= sd_T, n2i= N_T, data = data, append = TRUE, measure = "SMDH", var.names=c("SMDH","v_SMDHq"))
+
+# Clean data up a bit. We don't need all columns
+data <- data %>% 
+select(study, author, year, class, order, family, genus, species, variable, fitness.estimate, treatment, estimated.increase.in.harm, X_C, sd_C, N_C, X_T, sd_T, N_T, SMDH, v_SMDHq, SME_index, Male.measure, Female.measure, SCR.SCI, Sperm.compet, Harm_type, Parental.care , Gestation, depend, shared.control, source)
