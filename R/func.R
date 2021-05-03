7994e6d14b24eef30b0337317a05ca820c0c6a7f
Daniel Noble <daniel.wa.noble@gmail.com>
2021-05-03 11:45:57 +1000

    Added in a new p-value function to make it a little prettier

--- R/func.R
+++ R/func.R
@@ -57,3 +57,12 @@ pred_interval <- function(model, tval){
   
   return(tmp)
 }
+
+
+p_value <- function(x){
+  if(x <= 0.0001) {tmp = "< 0.0001"}
+  if(x <= 0.001 & x >= 0.0001) {tmp ="< 0.001"}
+  if(x <= 0.01 & x >= 0.001) {tmp ="< 0.01"}
+  if(x >= 0.01) {tmp = round(x, digits =2)}
+  return(tmp)
+}
